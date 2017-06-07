0edf86c6907e585b255195effac3fa075150718f
Alice Chen <alicechen118@gmail.com>
2017-06-06 19:50:21 -0700

    Passed all rspecs in board

--- w2d2/skeleton/lib/board.rb
+++ w2d2/skeleton/lib/board.rb
@@ -16,7 +16,7 @@ class Board
   end
 
   def valid_move?(start_pos)
-    if start_pos > 13 || start_pos < 1
+    if start_pos > 12 || start_pos < 1
       raise "Invalid starting cup"
     end
   end
@@ -30,16 +30,21 @@ class Board
       add_idx = 0 if add_idx > 13
       if add_idx == 6
         @cups[6] << stones.pop if current_player_name == @name1
-      elsit add_idx == 13
+      elsif add_idx == 13
         @cups[13] << stones.pop if current_player_name == @name2
       else
         @cups[add_idx] << stones.pop
       end
     end
+    render
+    next_turn(add_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
+    return :prompt if ending_cup_idx == 6 || ending_cup_idx == 13
+    return :switch if @cups[ending_cup_idx].length == 1
+    return ending_cup_idx
   end
 
   def render
@@ -51,8 +56,13 @@ class Board
   end
 
   def one_side_empty?
+    (0..5).each.all? { |idx| @cups[idx].empty? } ||
+    (7..12).each.all? { |idx| @cups[idx].empty? }
   end
 
   def winner
+    return @name1 if @cups[6].length > @cups[13].length
+    return @name2 if @cups[6].length < @cups[13].length
+    return :draw
   end
 end
