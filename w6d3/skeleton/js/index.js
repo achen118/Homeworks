3e35528526c845a8ba922e501d54ee6f8616009f
Alice Chen <alicechen118@gmail.com>
2017-07-04 21:41:35 -0700

    Complete w6d3 first ajax request hw

---
+++ w6d3/skeleton/js/index.js
@@ -0,0 +1,17 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+$.ajax({
+  type: "GET",
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  dataType: "json",
+  success: function(response) {
+    console.log(response);
+  },
+  error: function(msg) {
+    console.log(msg);
+  }
+});
+
+// Add another console log here, outside your AJAX request
+console.log("Goodbye");
